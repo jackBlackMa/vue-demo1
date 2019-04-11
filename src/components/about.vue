a7426e185b24cfc2d157d7f3f77273a5eb8b8b71
jackBlackMa <15618068205@163.com>
2019-04-11 17:36:30 +0800

    feat(vue-vuex):

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,10 +1,28 @@
 <template>
-    <div>介绍页123</div>
+  <div>
+    <div>
+      <button @click='handleAdd'>++</button>
+      <button @click='handleMin'>--</button>
+    </div>
+    <div>{{count}}</div>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  methods: {
+    handleAdd() {
+        this.$store.commit('increment')
+    },
+    handleMin(){
+         this.$store.commit('decrease')
+    }
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  }
+};
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import App from './App.vue';
 import Vue from 'vue';
 import VueRouter from 'vue-router'
 import RouterConf from './router/router'
-
+import Vuex from 'vuex'
 // MyPlugin.install = (Vue,options){
 
 // }
 
+// 使用router插件
 Vue.use(VueRouter)
 
+// 使用vuex插件
+Vue.use(Vuex)
 
 
 const router = new VueRouter(RouterConf)
@@ -28,8 +31,23 @@ router.afterEach((to,from,next)=>{
     window.scrollTo(0,0)
 })
 
+const store = new Vuex.Store({
+    state: {
+        count: 0,
+    },
+    mutations:{
+        increment(state){
+            state.count++;
+        },
+        decrease(state){
+            state.count--;
+        }
+    }
+})
+
 new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(App)
 })
